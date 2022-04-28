2d8a749e4bde14bf77abf258abe2b3d5606b0470
Alexey Borisenko <frozenpriest@mail.ru>
2022-04-28 21:23:55 +0300

    #6 empty viewmodel, small cleanup

---
+++ app/src/main/java/ru/frozenpriest/pokebase/presentation/screens/pokemon/owned/OwnedPokemonsViewModel.kt
@@ -0,0 +1,6 @@
+package ru.frozenpriest.pokebase.presentation.screens.pokemon.owned
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class OwnedPokemonsViewModel @Inject constructor() : ViewModel()
